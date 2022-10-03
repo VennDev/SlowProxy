f2693cb4b5915bed70ba3df28c7181d281ee1ae9
VennV <111500380+VennDev@users.noreply.github.com>
2022-10-03 11:57:59 +0700

    Add files via upload

---
+++ src/Proxy.js
@@ -0,0 +1,12 @@
+const bindIp = ''
+const bindPort = 1080
+const serverIp = ''
+const serverPort = 443
+
+function enable() {
+    console.log("Proxy is enabled!")
+}
+
+function disable() {
+    console.log("Proxy is disabled!")
+}
